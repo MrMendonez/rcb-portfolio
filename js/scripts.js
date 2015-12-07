52b3c6cda2c2cac48bb1e3e1076c1a3a0a2403cb
MrMendonez <dmendonez@gmail.com>
2015-12-06 19:05:52 -0500

    removed js script since none was needed.

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,5 @@
 // Scripts for My Portfolio
 
-$(document).ready(function(){
-  var carrotUp = $("<span>").addClass("glyphicon glyphicon-triangle-top");
-  var carrotDown = $("<span>").addClass("glyphicon glyphicon-triangle-bottom");
+// $(document).ready(function(){
 
-  $(document).on("click", ".read-more", function(){
-    $(this).replaceWith($("<span>").addClass("read-less").append("Read less ").append(carrotUp));
-    })
-
-  $(document).on("click", ".read-less", function(){
-    $(this).replaceWith($("<span>").addClass("read-more").append("Read more ").append(carrotDown));
-  })
-
-});
+// });
