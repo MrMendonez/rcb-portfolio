533499c3d2e0e1ab13f89f5aa42ca77d81a84e67
MrMendonez <dmendonez@gmail.com>
2015-12-21 20:21:26 -0500

    added mouse over animations on img links and youtube videos

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 // Scripts for My Portfolio
 
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// });
+  // Pulse animation on mouse hover for img links and youtube videos
+  $(".pulse-on-mouseenter").on("mouseenter", function() {
+    $(this).toggleClass("animated pulse");
+  }).on("mouseleave", function() {
+    $(this).toggleClass("animated pulse");
+  })
+}); // end document ready function
