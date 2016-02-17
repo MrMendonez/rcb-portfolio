d4db376d79cab68bf8db77f91cfff5477130acf5
MrMendonez <dmendonez@gmail.com>
2016-02-17 08:31:50 -0500

    dryed up mouseenter pulse button code for cards in projects section

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -7,47 +7,17 @@ $(document).ready(function(){
   $("#down-arrow-mobile").hide().delay(3000).fadeIn(500);
 
   // Pulse animation on mouse hover for img links and buttons
-  // DRY this code up
   $(".pulse-on-mouseenter").on("mouseenter", function() {
     $(this).toggleClass("animated pulse");
   }).on("mouseleave", function() {
     $(this).toggleClass("animated pulse");
   });
 
-  $(".pulse-on-db").on("mouseenter", function() {
-    $("#db-btn").toggleClass("animated pulse");
+  // Pulse btn on mouse enter on project cards
+  $(".pulse-activator").on("mouseenter", function() {
+    $(this).find("a").toggleClass("animated pulse");
   }).on("mouseleave", function() {
-    $("#db-btn").toggleClass("animated pulse");
-  });
-
-  $(".pulse-on-fi").on("mouseenter", function() {
-    $("#fi-btn").toggleClass("animated pulse");
-  }).on("mouseleave", function() {
-    $("#fi-btn").toggleClass("animated pulse");
-  });
-
-  $(".pulse-on-so").on("mouseenter", function() {
-    $("#so-btn").toggleClass("animated pulse");
-  }).on("mouseleave", function() {
-    $("#so-btn").toggleClass("animated pulse");
-  });
-
-  $(".pulse-on-booked").on("mouseenter", function() {
-    $("#booked-btn").toggleClass("animated pulse");
-  }).on("mouseleave", function() {
-    $("#booked-btn").toggleClass("animated pulse");
-  });
-
-  $(".pulse-on-checked").on("mouseenter", function() {
-    $("#checked-btn").toggleClass("animated pulse");
-  }).on("mouseleave", function() {
-    $("#checked-btn").toggleClass("animated pulse");
-  });
-
-  $(".pulse-on-lwd").on("mouseenter", function() {
-    $("#lwd-btn").toggleClass("animated pulse");
-  }).on("mouseleave", function() {
-    $("#lwd-btn").toggleClass("animated pulse");
+    $(this).find("a").toggleClass("animated pulse");
   });
 
   // Parallax Effect
