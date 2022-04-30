57b782af0a94a0c1380a7f152edfef0ec0914c14
Darryl Mendonez <dmendonez@mdsol.com>
2022-04-30 01:31:10 -0400

    added console.log welcome msg

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,20 @@
-// Scripts for My Portfolio
-
 $(document).ready(function() {
 
-  // Pulse animation on mouse hover for img links and buttons
   $(".pulse-on-mouseenter").on("mouseenter", function() {
     $(this).toggleClass("animated pulse");
   }).on("mouseleave", function() {
     $(this).toggleClass("animated pulse");
   });
 
-  // Parallax Effect
   $('.parallax').parallax();
-
-  // Scrollspy Effect
   $('.scrollspy').scrollSpy({scrollOffset: 64});
 
-  // Modal
-  // $('.modal-trigger').leanModal();
+  console.log(
+    "%cHello! Welcome to Darryl's portfolio 🙂 Thanks for stopping by and have a great rest of your day!",
+    `background: linear-gradient(#E66465, #9198E5);
+    padding: 1rem;
+    color: white;
+    border-radius: .5em`
+  )
 
-}); // end document ready function
+});
